e0cc99fa30a56039396c4f099b0b14255696423f
TariqKotwal <tariqkotwal@gmail.com>
2021-12-06 19:00:26 +0530

    Increase code coverage

--- tests/php/TestAmpStatsBlock.php
+++ tests/php/TestAmpStatsBlock.php
@@ -26,7 +26,8 @@ class TestAmpStatsBlock extends TestCase {
 	 * Getting instance of the class for rest of the tests.
 	 */
 	public function setUp():void { // @codingStandardsIgnoreLine
-		$this->instance = AmpStatsBlocks::get_instance();
+		$this->instance      = AmpStatsBlocks::get_instance();
+		$this->template_mode = 'standard';
 	}
 
 	/**
@@ -54,7 +55,6 @@ class TestAmpStatsBlock extends TestCase {
 		$validated_urls          = new \stdClass;
 		$validated_urls->publish = 2;
 		$total_errors            = 0;
-		$template_mode           = 'standard';
 
 		\WP_Mock::userFunction( 'wp_count_posts' )
 			->with( 'amp_validated_url' )
@@ -70,14 +70,9 @@ class TestAmpStatsBlock extends TestCase {
 		$amp_options_manager->shouldReceive( 'get_option' )
 			->zeroOrMoreTimes()
 			->with( 'theme_support' )
-			->andReturn( $template_mode );
-		$this->assertEquals( $template_mode, $amp_options_manager->get_option( 'theme_support' ) );
+			->andReturn( $this->template_mode );
 
 		$amp_stats_blocks = Mockery::mock( AmpStatsBlocks::class );
-		$amp_stats_blocks->shouldReceive( 'get_template_mode' )
-			->zeroOrMoreTimes()
-			->andReturn( $template_mode );
-		$this->assertEquals( $template_mode, $amp_stats_blocks->get_template_mode() );
 
 		$amp_stats = $this->instance->get_amp_stats();
 		$this->assertEquals( $total_errors, $amp_stats['total_validated_errors'] );
@@ -97,8 +92,12 @@ class TestAmpStatsBlock extends TestCase {
 		$template_mode_string = sprintf(
 			/* translators: %s: current template mode */
 			__( 'The template mode is %s.' ),
-			$template_mode
+			$this->template_mode
 		);
+
+		$amp_stats_blocks->shouldReceive( 'gutenberg_amp_dynamic_render_callback' )
+			->zeroOrMoreTimes();
+
 		$render_template_mode_show = $this->instance->gutenberg_amp_dynamic_render_callback( array( 'show' => true ) );
 		$this->assertStringContainsString( $validated_urls_string, $render_template_mode_show );
 		$this->assertStringContainsString( $validation_errors_string, $render_template_mode_show );
@@ -117,13 +116,6 @@ class TestAmpStatsBlock extends TestCase {
 		$this->assertStringContainsString( $validation_errors_string, $amp_block_body );
 		$this->assertStringContainsString( $template_mode_string, $amp_block_body );
 
-		$amp_stats_blocks->shouldReceive( 'get_total_validated_urls' )
-			->zeroOrMoreTimes();
-		$this->assertEquals( $validated_urls->publish, $this->instance->get_total_validated_urls() );
-
-		$amp_stats_blocks->shouldReceive( 'get_total_validation_errors' )
-			->zeroOrMoreTimes();
-		$this->assertEquals( $total_errors, $this->instance->get_total_validation_errors() );
 	}
 
 	/**
@@ -153,6 +145,118 @@ class TestAmpStatsBlock extends TestCase {
 			->with( 'amp-gutenberg/amp-dynamic-stats', $reg_block_args );
 
 		$this->instance->gutenberg_amp_dynamic_stats();
+	}
+
+	/**
+	 * Test get_amp_stats.
+	 *
+	 * @covers \XWP\BlockScaffolding\AmpStatsBlocks::get_amp_stats()
+	 */
+	public function test_get_amp_stats() {
+		$validated_urls          = new \stdClass;
+		$validated_urls->publish = 2;
+
+		\WP_Mock::userFunction( 'wp_count_posts' )
+			->with( 'amp_validated_url' )
+			->zeroOrMoreTimes()
+			->andReturn( $validated_urls );
+		$amp_options_manager = Mockery::mock( 'alias:\AMP_Options_Manager' );
+		$amp_options_manager->shouldReceive( 'get_option' )
+			->zeroOrMoreTimes()
+			->with( 'theme_support' )
+			->andReturn( $this->template_mode );
+		$amp_stats_blocks = Mockery::mock( AmpStatsBlocks::class );
+		$amp_stats_blocks->shouldReceive( 'get_amp_stats' )
+			->zeroOrMoreTimes()
+			->andReturn(
+				array(
+					'total_validated_errors' => 0,
+					'total_validated_urls'   => 2,
+					'template_mode'          => 'standard',
+				)
+			);
+		$amp_stats = $this->instance->get_amp_stats();
+	}
+
+	/**
+	 * Test get_total_validation_errors.
+	 *
+	 * @covers \XWP\BlockScaffolding\AmpStatsBlocks::get_total_validation_errors()
+	 */
+	public function test_get_total_validation_errors() {
+		$amp_stats_blocks = Mockery::mock( AmpStatsBlocks::class );
+		$amp_stats_blocks->shouldReceive( 'get_total_validation_errors' )
+			->zeroOrMoreTimes()
+			->andReturn( 0 );
+		$amp_stats = $this->instance->get_total_validation_errors();
+	}
+
+	/**
+	 * Test get_template_mode.
+	 *
+	 * @covers \XWP\BlockScaffolding\AmpStatsBlocks::get_template_mode()
+	 */
+	public function test_get_template_mode() {
+		$amp_options_manager = Mockery::mock( 'alias:\AMP_Options_Manager' );
+		$amp_options_manager->shouldReceive( 'get_option' )
+			->zeroOrMoreTimes()
+			->with( 'theme_support' )
+			->andReturn( $this->template_mode );
+		$amp_stats_blocks = Mockery::mock( AmpStatsBlocks::class );
+		$amp_stats_blocks->shouldReceive( 'get_template_mode' )
+			->zeroOrMoreTimes()
+			->andReturn( $this->template_mode );
+		$amp_stats = $this->instance->get_template_mode();
+	}
+
+	/**
+	 * Test get_total_validated_urls.
+	 *
+	 * @covers \XWP\BlockScaffolding\AmpStatsBlocks::get_total_validated_urls()
+	 */
+	public function test_get_total_validated_urls() {
+		$validated_urls          = new \stdClass;
+		$validated_urls->publish = 2;
+
+		\WP_Mock::userFunction( 'wp_count_posts' )
+			->with( 'amp_validated_url' )
+			->zeroOrMoreTimes()
+			->andReturn( $validated_urls );
+		$amp_stats_blocks = Mockery::mock( AmpStatsBlocks::class );
+		$amp_stats_blocks->shouldReceive( 'get_total_validated_urls' )
+			->zeroOrMoreTimes()
+			->andReturn( 2 );
+		$amp_stats = $this->instance->get_total_validated_urls();
+	}
+
+	/**
+	 * Test amp_stats_get_block_body.
+	 *
+	 * @covers \XWP\BlockScaffolding\AmpStatsBlocks::amp_stats_get_block_body()
+	 */
+	public function test_amp_stats_get_block_body() {
+
+		$validated_urls          = new \stdClass;
+		$validated_urls->publish = 2;
+
+		\WP_Mock::userFunction( 'wp_count_posts' )
+			->with( 'amp_validated_url' )
+			->zeroOrMoreTimes()
+			->andReturn( $validated_urls );
+
+		$amp_options_manager = Mockery::mock( 'alias:\AMP_Options_Manager' );
+		$amp_options_manager->shouldReceive( 'get_option' )
+			->zeroOrMoreTimes()
+			->with( 'theme_support' )
+			->andReturn( $this->template_mode );
+
+		$amp_stats        = $this->instance->get_amp_stats();
+		$amp_stats_blocks = Mockery::mock( AmpStatsBlocks::class );
+		$amp_stats_blocks->shouldReceive( 'amp_stats_get_block_body' )
+			->with( $amp_stats, array( 'show' => true ) )
+			->zeroOrMoreTimes()
+			->andReturn( 2 );
+		$amp_stats = $this->instance->amp_stats_get_block_body( $amp_stats, array( 'show' => true ) );
 	}
 
 	/**
